4a0f1a2e8de23c71e54a33cfbbb99fd00df08f9c
madhuri tummanapelli <104639282+buji2124@users.noreply.github.com>
2022-05-15 17:18:55 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World from deveops");
+    System.out.println("Hello World from deveops 2");
     }
 }
